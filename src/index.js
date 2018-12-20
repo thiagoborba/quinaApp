d5a57d4098113bb818749059044a9e374c8e4299
Thiago Borba <thborba1@gmail.com>
2018-12-20 10:24:14 -0200

    importado as config do db e adicionado o bodyParser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import routes from './routes';
 import cors from 'cors';
+import './config/db'
+import bodyParser from 'body-parser'
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors()) //sempre deve ser setado antes das rotas
 app.use('/api', routes);
 app.use(express.static(`${__dirname}/client`))
