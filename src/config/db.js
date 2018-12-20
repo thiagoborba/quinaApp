7b2e3d8f9b118deff1bdd049ab0bf279e8d10eeb
Thiago Borba <thborba1@gmail.com>
2018-12-20 10:23:34 -0200

    refatorado o codigo e adicionado  useNewUrlParser: true nas options

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,19 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import '../models/Task';
 
-const dbURI = "mongodb+srv://thiago:1LFQ8KkkGRlwOfeb@cluster0-tzate.mongodb.net/test?retryWrites=true"
+const dbURI = "mongodb+srv://thiago:1LFQ8KkkGRlwOfeb@cluster0-tzate.mongodb.net/test?retryWrites=true";
 
 const options = {
   reconnectTries: Number.MAX_VALUE,
-  poolSize: 10
+  poolSize: 10,
+  useNewUrlParser: true
 };
 
-mongoose.connect(dbURI, options).then(
-  () => {
+mongoose.connect(dbURI, options)
+  .then(() => {
     console.log("Database connection established!");
   },
   err => {
     console.log("Error connecting Database instance due to: ", err);
   }
-);
-
-// require any models
-
-require("../models/Task");
+)
