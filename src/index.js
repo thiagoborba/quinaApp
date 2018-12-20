0af4b96d25b73e0aea946c887b222ea79913360c
Thiago Borba <thborba1@gmail.com>
2018-12-20 11:08:09 -0200

    adicionado o cors no caminho das rotas e tirado do index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import routes from './routes';
-import cors from 'cors';
 import './config/db'
 import bodyParser from 'body-parser'
 
@@ -8,14 +7,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors()) //sempre deve ser setado antes das rotas
 app.use('/api', routes);
 app.use(express.static(`${__dirname}/client`))
-
-
-app.get('/', (req, res) => {
-  res.send('get working');
-});
 
 const port = 3001;
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import postagens from './postagens';
 import comentarios from './comentarios';
+import cors from 'cors'
 
 const router = express.Router();
 
+router.use(cors())
 router.use(postagens);
 router.use(comentarios);
 
