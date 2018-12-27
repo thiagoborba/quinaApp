c7c8d33207da82ac4036aaa6f10c1bedc95b5339
thiagoborba <thborba1@gmail.com>
2018-12-27 21:10:47 -0200

    importado configuracao do mongodb

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import routes from './routes';
-import './config/db'
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/api', routes);
-app.use(express.static(`${__dirname}/client`))
+app.use(express.static(`${__dirname}/client`));
 
 const port = 3001;
 
