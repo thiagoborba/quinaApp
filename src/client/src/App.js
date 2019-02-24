150104881b37bce741ba97f7af1a63b770166fd8
thiagoborba <thborba1@gmail.com>
2019-02-24 15:25:25 -0300

    feat:  AuthorBox Created

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,68 +1,7 @@
 import React, { Component } from 'react'
-import './css/pure-min.css'
-import './css/side-menu.css'
-import TableInput from './componentes/TableInput'
-import fetch from 'node-fetch'
-
-const APP_SERVER_URL = 'http://localhost:3000/api/authors'
+import AuthorBox from './componentes/AuthorBox'
 
 class App extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      lista: [],
-      name: '',
-      email: '',
-      password: '',
-    }
-  }
-
-  componentDidMount () {
-    this.fetchTableContent()
-  }
-
-  fetchTableContent = async () => {
-    await fetch(APP_SERVER_URL)
-      .then(res => res.json())
-      .then(data => this.setState({
-        lista: data,
-      }))
-  }
-
-  enviaForm = async (e) => {
-    e.preventDefault()
-
-    const { name, email, password } = this.state
-
-    await fetch(APP_SERVER_URL, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email, password }),
-    })
-      .then(res => res.json())
-      .then(json => console.log('resposta', json))
-      .catch(err => console.log(err))
-      // NAO ESTA ATUALIZANDO O COMPONENTE
-
-    this.fetchTableContent()
-  }
-
-  setName = (e) => {
-    this.setState({ name: e.currentTarget.value })
-  }
-
-  setEmail = (e) => {
-    this.setState({ email: e.currentTarget.value })
-  }
-
-  setPassword = (e) => {
-    this.setState({ password: e.currentTarget.value })
-  }
-
   render () {
     return (
       <div id='layout'>
@@ -75,7 +14,7 @@ class App extends Component {
             <ul className='pure-menu-list'>
               <li className='pure-menu-item'><a href='#' className='pure-menu-link'>Home</a></li>
               <li className='pure-menu-item'><a href='#' className='pure-menu-link'>Autor</a></li>
-              <li className='pure-menu-item'><a href='#' className='pure-menu-link'>Livro</a></li>
+              <li className='pure-menu-item'><a href='#' className='pure-menu-link'>Livros</a></li>
             </ul>
           </div>
         </div>
@@ -83,59 +22,7 @@ class App extends Component {
           <div className='header'>
             <h1>Cadastro de Autores</h1>
           </div>
-          <div className='content' id='content'>
-            <div className='pure-form pure-form-aligned'>
-              <form className='pure-form pure-form-aligned' onSubmit={this.enviaForm} method='post'>
-                <TableInput
-                  id='name'
-                  type='text'
-                  value={this.state.name}
-                  onChange={this.setName}
-                  label='Name'
-                />
-                <TableInput
-                  id='email'
-                  type='email'
-                  value={this.state.email}
-                  onChange={this.setEmail}
-                  label='Email'
-                />
-                <TableInput
-                  id='password'
-                  type='password'
-                  value={this.state.password}
-                  onChange={this.setPassword}
-                  label='Password'
-                />
-                <div className='pure-control-group'>
-                  <label></label>
-                  <button type='submit' className='pure-button pure-button-primary'>Gravar</button>
-                </div>
-              </form>
-            </div>
-            <div>
-              <table className='pure-table'>
-                <thead>
-                  <tr>
-                    <th>name</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.lista.authors && this.state.lista.authors.map(autor => {
-                      return (
-                        <tr key={autor._id}>
-                          <td>{autor.name}</td>
-                          <td>{autor.email}</td>
-                        </tr>
-                      )
-                    })
-                  }
-                </tbody>
-              </table>
-            </div>
-          </div>
+          <AuthorBox />
         </div>
       </div>
     )
--- src/client/src/componentes/TableInput.js
+++ src/client/src/componentes/TableInput.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import '../css/pure-min.css'
+import '../css/side-menu.css'
 
 class TableInput extends Component {
   render () {
     return (
       <div className="pure-control-group">
-        <label htmlFor={this.props.id}>{this.props.label}</label>
-        <input id={this.props.id} type={this.props.type} value={this.props.value} onChange={this.props.onChange} />
+        <label
+          htmlFor={this.props.id}
+        >
+          {this.props.label}
+        </label>
+        <input
+          id={this.props.id}
+          type={this.props.type}
+          value={this.props.value}
+          onChange={this.props.onChange} />
       </div>
     )
   }
